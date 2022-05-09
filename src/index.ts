edbb692168e482f46fa4baaff8bd49b77ae1ac03
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2022-05-09 17:37:47 -0300

    feat(apollo): add apollo server playground

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { ApolloServer } from 'apollo-server'
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
 import { schema } from './schema'
 import config from './config'
 
-export const server = new ApolloServer({ schema })
+export const server = new ApolloServer({
+  schema,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()]
+})
 
 server
   .listen({ host: config.HOST, port: config.PORT })
